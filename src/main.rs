57f5269ed3c97b3125b194c296e89dc5fe5b616a
Halffd <guhalfeld@hotmail.com>
2025-01-20 19:21:32 -0300

    traits, hex format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 fn abort() -> ! {
     panic!("This function will never return!");
 }
+fn check_value(x: i32) -> i32 {
+    if x < 0 {
+        abort(); // This will never return.
+    }
+    x
+}
 fn main() {
-    println!("Hello, world!");
-    abort();
+    println!("{:x}", 1692134);
+    println!("{:o}", check_value(5));
+    check_value(-20);
+    // std::procechess::exit(1);
+    //    abort();
 }
