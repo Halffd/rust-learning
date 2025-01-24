53160f612c97e9d022bf1e0c84f61275130aa48f
Halffd <guhalfeld@hotmail.com>
2025-01-23 22:04:30 -0300

    out of bound

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let arr = ["a", "b"];
-
-    // Attempt to access an out-of-bounds index
-    let index = 100;
-
-    // Using .get() for safe access
-    match arr.get(index) {
-        Some(value) => println!("Value at index {}: {}", index, value),
-        None => println!("Index {} is out of bounds.", index),
-    }
+    let v = vec![1, 2, 3, 4];
+    v[99];
 }
